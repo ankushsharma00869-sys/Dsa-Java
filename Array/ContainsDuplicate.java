6a9f8ae1bb8499367efd1ee5e00879cef88a31b3
Ankush Sharma <ankushsharma00869@gmail.com>
2026-02-22 08:47:52 +0530

    solved Contains Duplicate using hashset

---
+++ Array/ContainsDuplicate.java
@@ -0,0 +1,17 @@
+package Array;
+import java.util.HashSet;
+public class ContainsDuplicate {
+     public boolean containsDuplicate(int[] nums) {
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        
+        return false;
+    }
+}
